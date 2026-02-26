222274f8437749c448ba123351d4e01c8703c7f7
tvato <tvat0@protonmail.com>
2026-02-26 09:12:52 +0200

    added prepackaged database file

--- app/src/main/java/eu/tvato/lempie/database/LempieDatabase.kt
+++ app/src/main/java/eu/tvato/lempie/database/LempieDatabase.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import eu.tvato.lempie.LempieApplication
 import eu.tvato.lempie.database.datetimeformat.DateTimeFormat
 import eu.tvato.lempie.database.datetimeformat.DateTimeFormatDao
 import eu.tvato.lempie.database.settings.SettingsDao
