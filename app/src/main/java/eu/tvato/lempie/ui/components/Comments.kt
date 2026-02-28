361655b741b12cf34c380c81cff354e5009fd841
tvato <tvat0@protonmail.com>
2026-02-28 19:49:13 +0200

    added colored lines to indicate comment tree structure

--- app/src/main/java/eu/tvato/lempie/ui/components/Comments.kt
+++ app/src/main/java/eu/tvato/lempie/ui/components/Comments.kt
@@ -13,10 +13,17 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import eu.tvato.lempie.R
 import eu.tvato.lempie.comment.CommentView
+import eu.tvato.lempie.ui.previewdata.previewCommentViews
+import eu.tvato.lempie.ui.theme.LemPieTheme
+import eu.tvato.lempie.ui.theme.Theme
 import eu.tvato.lempie.utils.parseIsoDate
 
 @Composable
@@ -27,20 +34,41 @@ fun CommentRow(
     modifier: Modifier = Modifier,
     noPadding: Boolean = false
 ) {
-    val paddingValue = if(noPadding) 0 else comment?.comment?.path?.split(".")?.size?.minus(2)
+    val paddingValue = if(noPadding) 0 else comment?.comment?.path?.split(".")?.size?.minus(2) ?: 0
+    val startPadding = paddingValue.plus(8).times(paddingValue.plus(2).div(2)).dp
+    val colors = listOf(
+        // Change these colors, maybe use theme for this
+        Color.Red, Color.Green, Color.Blue, Color.Yellow
+    )
+    val lines = modifier
+        .background(MaterialTheme.colorScheme.primaryContainer)
+        .drawBehind {
+            for(i in 0..<paddingValue){
+                val xOffset = i.times(20f) + i.plus(1).times(2f)
+                drawLine(
+                    color = if(i < 4) colors[i] else colors[i - 4],
+                    start = Offset(x = xOffset, y = -1f),
+                    end = Offset(x = xOffset, y = size.height),
+                    strokeWidth = 2.dp.toPx()
+                )
+            }
+        }
     Column(
-        modifier = modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colorScheme.primaryContainer)
+        modifier = if(paddingValue != 0) {
+                lines
+            }else{
+                modifier
+                    .background(MaterialTheme.colorScheme.primaryContainer)
+            }
     ) {
         Row(
             horizontalArrangement = Arrangement.SpaceBetween,
             modifier = modifier
+                .fillMaxWidth()
                 .padding(
-                    start = (paddingValue?.times(20)?.plus(5))?.dp ?: 0.dp,
+                    start = startPadding,
                     end = 5.dp, top = 5.dp, bottom = 5.dp
                 )
-                .fillMaxWidth()
         ) {
             Text(
                 text = "${username}@${if(userInstance.split("/").size > 1) userInstance.split("/")[2] else userInstance}",
@@ -52,10 +80,15 @@ fun CommentRow(
             )
         }
         Text(
+            text = comment?.comment?.path.toString(),
+            color = MaterialTheme.colorScheme.onBackground,
+            modifier = modifier.padding(start = startPadding)
+        )
+        Text(
             text = comment?.comment?.content.toString(),
             modifier = modifier
                 .padding(
-                    start = (paddingValue?.times(20)?.plus(10))?.dp ?: 5.dp,
+                    start = startPadding,
                     top = 5.dp, bottom = 5.dp
                 )
                 .fillMaxWidth(),
@@ -71,7 +104,7 @@ fun CommentRow(
                 text = comment?.counts?.upvotes.toString(),
                 modifier = modifier
                     .padding(
-                        start = (paddingValue?.times(20)?.plus(5))?.dp ?: 0.dp
+                        start = startPadding
                     )
             )
             InteractionButton(
@@ -170,5 +203,61 @@ fun UserComment(
                 text = null
             )
         }
+    }
+}
+
+@Preview
+@Composable
+fun CommentRowPreview(){
+    LemPieTheme(theme = Theme.Dark) {
+        val comment = previewCommentViews[0]
+        CommentRow(
+            comment = comment,
+            username = comment.creator.name,
+            userInstance = comment.creator.actorId,
+            noPadding = false
+        )
+    }
+}
+
+@Preview
+@Composable
+fun CommentRowPreview2(){
+    LemPieTheme(theme = Theme.Dark) {
+        val comment = previewCommentViews[1]
+        CommentRow(
+            comment = comment,
+            username = comment.creator.name,
+            userInstance = comment.creator.actorId,
+            noPadding = false
+        )
+    }
+}
+
+@Preview
+@Composable
+fun CommentRowPreview3(){
+    LemPieTheme(theme = Theme.Dark) {
+        val comment = previewCommentViews[5]
+        CommentRow(
+            comment = comment,
+            username = comment.creator.name,
+            userInstance = comment.creator.actorId,
+            noPadding = false
+        )
+    }
+}
+
+@Preview
+@Composable
+fun CommentRowPreview4(){
+    LemPieTheme(theme = Theme.Dark) {
+        val comment = previewCommentViews[2]
+        CommentRow(
+            comment = comment,
+            username = comment.creator.name,
+            userInstance = comment.creator.actorId,
+            noPadding = false
+        )
     }
 }
