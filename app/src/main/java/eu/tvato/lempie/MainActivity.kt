f792d250d0d319dbcb6532f1a708e62fab3e2fb5
tvato <tvat0@protonmail.com>
2026-03-06 18:56:26 +0200

    app now retrieves theme from DataStore on start

--- app/src/main/java/eu/tvato/lempie/MainActivity.kt
+++ app/src/main/java/eu/tvato/lempie/MainActivity.kt
@@ -13,21 +13,39 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.lifecycleScope
+import eu.tvato.lempie.datastore.DataStoreRepository
 import eu.tvato.lempie.ui.screens.HomeScreen
 import eu.tvato.lempie.ui.theme.LemPieTheme
 import eu.tvato.lempie.ui.theme.Theme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            LemPieTheme(theme = Theme.Dark){
+            var theme by remember { mutableStateOf("Dark") }
+            LaunchedEffect(Unit) {
+                lifecycleScope.launch {
+                    DataStoreRepository(applicationContext).getTheme().collect {
+                        theme = it
+                    }
+                }
+            }
+            LemPieTheme(
+                theme = Theme.valueOf(theme)
+            ){
                 HomeScreen(
                     modifier = Modifier
                 )
